c3787718c1bdfc7572ad7c661b0d097530bfb449
Wan <pollypollywww@gmail.com>
2023-06-07 00:40:40 +0800

    更新admin.js

--- router/admin.js
+++ router/admin.js
@@ -190,12 +190,12 @@ router.get("/mem/orderList/:userId", async(req, res) => {
         let ongoingOrder = countOrder - completeOrder - invalidOrder;
 
         //console.log(result)
-        if(result.length > 0){
+        // if(result.length > 0){
             res.render("admin/mem_order_list.html", { logUserName, UserName, result, countOrder, completeOrder, invalidOrder, ongoingOrder });
-        }else{
-            let message = "該會員目前尚無訂單";
-            res.render("admin/mem_order_list.html", { logUserName, UserName, message, countOrder, completeOrder, invalidOrder, ongoingOrder });
-        }
+        // }else{
+        //     let message = "該會員目前尚無訂單";
+        //     res.render("admin/mem_order_list.html", { logUserName, UserName, message, countOrder, completeOrder, invalidOrder, ongoingOrder });
+        // }
             
     }catch(err){
         console.log(err);
