6dd152afe24a8a597962550b9e96303ed8e12277
Wan <pollypollywww@gmail.com>
2023-06-07 15:52:03 +0800

    更新complete

--- application/js/complete.js
+++ application/js/complete.js
@@ -24,7 +24,8 @@ $(function () {
             <h4>已收到您的訂單</h4>
             <h5>訂單編號：${orderId}</h5>
             <br>
-            <p>匯款帳號：國泰世華(013) 123456-654321</p>
+            <h5>匯款帳號</h5>
+            <h5>匯款帳號：國泰世華(013) 123456-654321</h5>
             <br>
             <p class="text-danger">請於 ${validDate} 20:00 前完成付款，經審查後如逾時將取消訂單</p>
             <p>完成匯款後，請至 <a class="toThisLink" href="/members/orderlist"><會員專區-訂單紀錄></a> 更新匯款資訊</p>
